838ff163318bbc965c40c6d1d1142bd792fcfb9d
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-04-01 03:29:24 +0330

    start user section module

---
+++ src/Modules/User/UserModule.Data/UserModuleBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserModule.Data;
+
+public static class UserModuleBootstrapper
+{
+    public static IServiceCollection InitUserModule(this IServiceCollection services,IConfiguration config)
+    {
+        services.AddDbContext<UserContext>(option =>
+        {
+            option.UseSqlServer(config.GetConnectionString("User_Context"));
+        });
+        return services;
+    }
+}
