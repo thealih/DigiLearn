1186afd41ae80490b693e00ffbd3f80b9a68e3eb
Ali Hashemi <alihashemi1381.ah@gmail.com>
2025-04-10 23:36:29 +0330

    update notification section

--- src/EndPoints/DigiLearn.Web/Pages/Profile/Notifications.cshtml.cs
+++ src/EndPoints/DigiLearn.Web/Pages/Profile/Notifications.cshtml.cs
@@ -1,12 +1,44 @@
+using DigiLearn.Web.Infrastructure;
+using DigiLearn.Web.Infrastructure.RazorUtils;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using UserModule.Core.Commands.Notifications.Delete;
+using UserModule.Core.Commands.Notifications.DeleteAll;
+using UserModule.Core.Queries._DTOs;
+using UserModule.Core.Services;
 
-namespace DigiLearn.Web.Pages.Profile
+namespace DigiLearn.Web.Pages.Profile;
+
+public class NotificationsModel : BaseRazorFilter<NotificationFilterParams>
 {
-    public class NotificationsModel : PageModel
+    private INotificationFacade _notificationFacade;
+
+    public NotificationsModel(INotificationFacade notificationFacade)
     {
-        public void OnGet()
+        _notificationFacade = notificationFacade;
+    }
+
+    public NotificationFilterResult FilterResult { get; set; }
+
+    public async Task OnGet()
+    {
+        FilterResult = await _notificationFacade.GetByFilter(new NotificationFilterParams
         {
-        }
+            IsSeen = null,
+            PageId = FilterParams.PageId,
+            Take = 6,
+            UserId = User.GetUserId()
+        });
+    }
+
+    public async Task<IActionResult> OnPostDeleteAll()
+    {
+        return await AjaxTryCatch(
+            () => _notificationFacade.DeleteAll(new DeleteAllNotificationCommand(User.GetUserId())));
+    }
+
+    public async Task<IActionResult> OnPostDeleteNotification(Guid id)
+    {
+        return await AjaxTryCatch(
+            () => _notificationFacade.Delete(new DeleteNotificationCommand(id,User.GetUserId())));
     }
 }
